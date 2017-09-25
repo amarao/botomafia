bb59aebeaa4daa2c22f38634e700accc2a738068
George Shuklin <george.shuklin@gmail.com>
2017-09-25 17:28:37 +0300

    Cleanup old Strategy before applying for new one

--- botomafia/roles.py
+++ botomafia/roles.py
@@ -7,19 +7,25 @@ class Role(object):
     def __init__(self, name, game, strategies=[]):
         self.name = name
         self.game = game
+        self.stategy = None
         if not strategies:
             self.strategies = [DefaultStrategy]
         else:
             self.strategies = strategies
-        self._switch_strategy(strategies[0])
+        self.strategy = self._switch_strategy(strategies[0])
         self.configure_role()
 
     def _switch_strategy(self, NewStrategy):
+
+        if NewStrategy != DefaultStrategy:
+            self._switch_strategy(DefaultStrategy)  # cleanup old functions
+
         for method_name in dir(NewStrategy):
             if method_name.startswith('_'):
                     continue
             function = getattr(NewStrategy, method_name).__func__
             setattr(self, method_name, types.MethodType(function, self))
+        return NewStrategy
 
     def __str__(self):
         return self.name
