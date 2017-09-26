b71166d25c9f491cd7e4fc6321ec43f53125d270
George Shuklin <george.shuklin@gmail.com>
2017-09-26 22:37:03 +0300

    Change the way strategy switches. All old methods and attributes not started from '_' will be wiped before switching to a new strategy.

--- botomafia/roles.py
+++ botomafia/roles.py
@@ -13,20 +13,18 @@ class Role(object):
         else:
             self.strategies = strategies
         self.strategy = self._switch_strategy(strategies[0])
-        self.first_strategy = strategies[0]
+        self.first_strategy = self.strategy
         self.initial_constants = {}
         self.configure_role()
 
     def _switch_strategy(self, NewStrategy):
-
-        if NewStrategy != DefaultStrategy:
-            self._switch_strategy(DefaultStrategy)  # cleanup old functions
-
+        for method_name in dir(self):
+            if not method_name.startswith('_'):
+                delattr(self, method_name)
         for method_name in dir(NewStrategy):
-            if method_name.startswith('_'):
-                    continue
-            function = getattr(NewStrategy, method_name).__func__
-            setattr(self, method_name, types.MethodType(function, self))
+            if not method_name.startswith('_'):
+                function = getattr(NewStrategy, method_name).__func__
+                setattr(self, method_name, types.MethodType(function, self))
         return NewStrategy
 
     def __str__(self):
